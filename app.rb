334a4d186f004b1889089199eeb2853b3876e505
Nicholas Powers <njpowers7915@gmail.com>
2018-06-04 00:58:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @team = 
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     
     erb :team
   end
